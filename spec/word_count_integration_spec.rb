6c4e007aeeb754268306719e260f9d1b08db46cd
Matt Eicshteadt <matteicshteadt@gmail.com>
2016-11-18 11:28:34 -0800

    add another test to determine if checkbox works.

--- spec/word_count_integration_spec.rb
+++ spec/word_count_integration_spec.rb
@@ -6,9 +6,17 @@ set(:show_exceptions, false)
 describe('the word_count path', {:type => :feature}) do
   it('counts the amount of times a word appears in a sentece') do
     visit('/')
-    fill_in('sentence', :with => 'Williamsburg cray paleo pop-up listicle')
+    fill_in('sentence', :with => 'Williamsburg cray paleo paleopop-up listicle')
     fill_in('search_word', :with => "paleo")
     click_button('Submit')
-    expect(page).to have_content('paleo appears 1 times in Williamsburg cray paleo pop-up listicle')
+    expect(page).to have_content('paleo appears 2 times in Williamsburg cray paleo paleopop-up listicle')
+  end
+  it('counts the amount of times a word appears in a sentece while checking the strict box') do
+    visit('/')
+    fill_in('sentence', :with => 'Williamsburg cray paleo paleopop-up listicle')
+    fill_in('search_word', :with => "paleo")
+     check('check[]')
+    click_button('Submit')
+    expect(page).to have_content('paleo appears 1 times in Williamsburg cray paleo paleopop-up listicle')
   end
 end
